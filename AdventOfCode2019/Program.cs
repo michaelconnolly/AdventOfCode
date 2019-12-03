922feb90de1c7b26aa16ee9eb4f244d51c98e7be
mc <mc@madronahouse.com>
2019-12-03 00:31:01 -0800

    Day 2

--- AdventOfCode2019/Program.cs
+++ AdventOfCode2019/Program.cs
@@ -7,7 +7,12 @@ namespace AdventOfCode2019 {
 
         static void Main(string[] args) {
 
-            DayOne();
+            //Day1();
+            Day2();
+
+            // Keep the console window open.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
         }
 
         static int FuelRequired(int inputValue) {
@@ -30,7 +35,7 @@ namespace AdventOfCode2019 {
             return fuelRequired;
         }
 
-        static void DayOne() {
+        static void Day1() {
 
             string fileName = "C:\\dev\\AdventOfCode\\AdventOfCode2019\\day1_input.txt";
             string[] lines = System.IO.File.ReadAllLines(fileName);
@@ -66,10 +71,94 @@ namespace AdventOfCode2019 {
 
             Console.WriteLine("Total number of lines: " + numberOfLines.ToString());
             Console.WriteLine("Total fuel required, nested: " + totalFuelRequired.ToString());
+        }
 
-            // Keep the console window open.
-             Console.WriteLine("Press any key to exit.");
-            System.Console.ReadKey();
+        static void Day2() {
+
+            string fileName = "C:\\dev\\AdventOfCode\\AdventOfCode2019\\day2_input.txt";
+            string content = System.IO.File.ReadAllText(fileName);
+
+            // Some of the example test cases.
+            //string content = "1,0,0,0,99";
+            //string content = "1,1,1,4,99,5,6,0,99";
+
+            string[] opCodes = content.Split(",");
+
+            // Once you have a working computer, the first step is to restore the gravity assist program (your puzzle input) to the "1202 program alarm" state 
+            // it had just before the last computer caught fire. To do this, before running the program, replace position 1 with the value 12 and replace
+            // position 2 with the value 2. What value is left at position 0 after the program halts?
+            opCodes[1] = "12";
+            opCodes[2] = "2";
+
+            // Display the file contents.
+            System.Console.WriteLine("Contents of file = ");
+
+            // Day 2, Part 1.
+            foreach (string opCode in opCodes) {
+                Console.WriteLine("\t" + opCode);
+            }
+            int numberOfOpCodes = opCodes.Length;
+            Console.WriteLine("Total number of opCodes: " + numberOfOpCodes.ToString());
+
+            bool keepGoing = true;
+            int valueLocationA, valueLocationB, valueLocationC;
+            int valueA, valueB, valueC;
+
+            for (int i=0; i<numberOfOpCodes; i=i+4) {
+
+                if (keepGoing) {
+
+                    switch (opCodes[i]) {
+
+                        case "1":
+
+                            // Opcode 1 adds together numbers read from two positions and stores the result in a third position.The three integers immediately after the opcode
+                            // tell you these three positions -the first two indicate the positions from which you should read the input values, and the third indicates the position
+                            // at which the output should be stored.
+                            valueLocationA = Convert.ToInt32(opCodes[i + 1]);
+                            valueLocationB = Convert.ToInt32(opCodes[i + 2]);
+                            valueLocationC = Convert.ToInt32(opCodes[i + 3]);
+
+                            valueA = Convert.ToInt32(opCodes[valueLocationA]);
+                            valueB = Convert.ToInt32(opCodes[valueLocationB]);
+                            valueC = (valueA + valueB);
+                            opCodes[valueLocationC] = valueC.ToString();
+
+                            break;
+
+                        case "2":
+
+                            // Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them. Again, the three integers after the opcode indicate
+                            // where the inputs and outputs are, not their values.
+                            valueLocationA = Convert.ToInt32(opCodes[i + 1]);
+                            valueLocationB = Convert.ToInt32(opCodes[i + 2]);
+                            valueLocationC = Convert.ToInt32(opCodes[i + 3]);
+
+                            valueA = Convert.ToInt32(opCodes[valueLocationA]);
+                            valueB = Convert.ToInt32(opCodes[valueLocationB]);
+                            valueC = valueA * valueB;
+                            opCodes[valueLocationC] = (valueC).ToString();
+
+                            break;
+
+                        case "99":
+
+                            keepGoing = false;
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                }
+            }
+
+            Console.WriteLine("Final list:");
+            foreach (string opCode in opCodes) {
+                Console.WriteLine("\t" + opCode);
+            }
+
+            Console.WriteLine("Final value in position 0: " + opCodes[0]);
         }
     }
 }
