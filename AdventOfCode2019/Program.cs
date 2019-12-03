25c87d3e217b2398a8fd0d317e2b8a5179969da2
mc <mc@madronahouse.com>
2019-12-02 22:17:14 -0800

    Day One, 2019

---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace AdventOfCode2019 {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            DayOne();
+        }
+
+        static int FuelRequired(int inputValue) {
+
+            int fuelRequired = ((int)Math.Truncate(((double)(inputValue / 3)))) - 2;
+
+            return fuelRequired;
+        }
+
+        static int FuelRequiredNested(int inputValue) {
+
+            int fuelRequired = FuelRequired(inputValue);
+            int additionalFuelRequired = FuelRequired(fuelRequired);
+            
+            while (additionalFuelRequired > 0) {
+                fuelRequired += additionalFuelRequired;
+                additionalFuelRequired = FuelRequired(additionalFuelRequired);
+            }
+
+            return fuelRequired;
+        }
+
+        static void DayOne() {
+
+            string fileName = "C:\\dev\\AdventOfCode\\AdventOfCode2019\\day1_input.txt";
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+            int numberOfLines = 0;
+            int totalFuelRequired = 0;
+         
+            // Display the file contents.
+            System.Console.WriteLine("Contents of file = ");
+
+           // Day 1, Part 1.
+           // Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+            foreach (string line in lines) {
+                int value = Convert.ToInt32(line);
+                int fuelRequired = FuelRequired(value);
+                numberOfLines++;
+                totalFuelRequired += fuelRequired;
+                Console.WriteLine("\t" + line + "; fuel required: " + fuelRequired.ToString());
+            }
+
+            Console.WriteLine("Total number of lines: " + numberOfLines.ToString());
+            Console.WriteLine("Total fuel required: " + totalFuelRequired.ToString());
+
+            // Day 1, Part 2.
+            numberOfLines = 0;
+            totalFuelRequired = 0;
+            foreach (string line in lines) {
+                int value = Convert.ToInt32(line);
+                int fuelRequired = FuelRequiredNested(value); 
+                numberOfLines++;
+                totalFuelRequired += fuelRequired;
+                Console.WriteLine("\t" + line + "; fuel required: " + fuelRequired.ToString());
+            }
+
+            Console.WriteLine("Total number of lines: " + numberOfLines.ToString());
+            Console.WriteLine("Total fuel required, nested: " + totalFuelRequired.ToString());
+
+            // Keep the console window open.
+             Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
