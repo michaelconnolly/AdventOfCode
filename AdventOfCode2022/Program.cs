651b9eb672b68edab145ac202e131b3d960dfe0f
mc <mc@madronahouse.com>
2022-12-03 10:31:12 -0800

    day 3

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 namespace AdventOfCode2022 {
@@ -11,8 +12,9 @@ namespace AdventOfCode2022 {
         static void Main(string[] args) {
 
             //Day1();
-            Day2();
-  
+            //Day2();
+            Day3();
+
             // Keep the console window open.
             Console.WriteLine("\nPress any key to exit.");
             System.Console.ReadKey();
@@ -26,7 +28,7 @@ namespace AdventOfCode2022 {
 
             ArrayList elves = new ArrayList();
             int caloriesCount = 0;
-     
+
             foreach (string line in lines) {
                 if (line == "") {
                     elves.Add(caloriesCount);
@@ -57,7 +59,7 @@ namespace AdventOfCode2022 {
                 else if (elf > max3CaloriesCount) {
                     max3CaloriesCount = elf;
                 }
-            } 
+            }
 
             // I have your answer for you.
             Console.WriteLine("Number of elves: " + elves.Count);
@@ -85,6 +87,64 @@ namespace AdventOfCode2022 {
 
             // I have your answer for you.
             Console.WriteLine("total score: " + totalScore);
+            Console.WriteLine("total score 2: " + totalScore2);
+        }
+
+        static void Day3() {
+
+            string fileName = dataFolderPath + "input_03.txt";
+            string[] lines = File.ReadAllLines(fileName);
+            Console.WriteLine("Size of input array: " + lines.Length);
+
+            // each line is one rucksack.
+            // rucksack has two compartments.
+            // each rucksack has a pile of stuff, one half in each compartment.
+            // there is one thing that is illegally represented in both compartments.
+            // that one thing has a score.  Tell me the score.
+
+            int totalScore = 0;
+ 
+            foreach (string line in lines) {
+
+                rucksack rucksack = new rucksack(line);
+                totalScore += rucksack.score();
+                rucksack.print();
+            }
+
+            // I have your answer for you.
+            Console.WriteLine("total score: " + totalScore);
+
+            // Part 2.
+            int countOfGroups = (lines.Length / 3);
+            Console.WriteLine("count of groups: " + countOfGroups);
+            int totalScore2 = 0;
+
+            for (int i = 0; i < countOfGroups; i++) {
+
+                // Figure out, in each group of 3, what is the one item that is common amongst all three.  
+
+                rucksack rucksack1 = new rucksack(lines[(i * 3) + 0]);
+                rucksack rucksack2 = new rucksack(lines[(i * 3) + 1]);
+                rucksack rucksack3 = new rucksack(lines[(i * 3) + 2]);
+
+                Dictionary<string, int> contents1 = rucksack1.contents();
+                Dictionary<string, int> contents2 = rucksack2.contents();
+                Dictionary<string, int> contents3 = rucksack3.contents();
+
+                bool foundIt = false;
+
+                foreach (string key in contents1.Keys) {
+
+                    if ((contents2.ContainsKey(key)) && (contents3.ContainsKey(key))) {
+                        totalScore2 += rucksack.ConvertStringToInt(key);
+                        foundIt = true;
+                        break;
+                    }
+                }
+
+                if (!foundIt) { throw new Exception(); }
+            }
+
             Console.WriteLine("total score 2: " + totalScore2);
         }
     }
---
+++ AdventOfCode2022/classes/rucksack.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2022 {
+
+    internal class rucksack {
+
+        public string compartment1;
+        public string compartment2;
+
+        public rucksack(string line) {
+
+            this.compartment1 = line.Substring(0, ((line.Length / 2)));
+            this.compartment2 = line.Substring(line.Length / 2);
+        }
+
+        public void print() {
+            Console.WriteLine("compartment 1: " + compartment1 + ", compartment 2: " + compartment2 +
+                ", bad item: " + this.badItem() + ", score: " + this.score());
+        }
+
+        public string badItem() {
+
+            Dictionary<string, int> thingsInCompartment1 = this.contents(1);
+
+            for (int i = 0; i < this.compartment2.Length; i++) {
+
+                string currentThing = this.compartment2.Substring(i, 1);
+                if (thingsInCompartment1.ContainsKey(currentThing)) {
+                    return currentThing;
+                }
+            }
+
+            throw new Exception();
+        }
+
+        static public int ConvertStringToInt(string input) {
+
+            char badItemChar = input[0];
+            int badItemInt = Convert.ToInt32(badItemChar);
+
+            if (badItemInt >= 97 && badItemInt <= 122) {
+                badItemInt -= 96;
+            }
+            else if (badItemInt >= 65 && badItemInt <= 90) {
+                badItemInt -= (65 - 27);
+            }
+            else {
+                throw new Exception();
+            }
+
+            return badItemInt;
+        }
+
+        public int score() {
+
+            string badItem = this.badItem();
+            return rucksack.ConvertStringToInt(badItem);
+        }
+
+        public Dictionary<string, int> contents() {
+
+            Dictionary<string, int> contents1 = this.contents(1);
+            Dictionary<string, int> contents2 = this.contents(2);
+
+            foreach (string key in contents2.Keys) {
+
+                if (contents1.ContainsKey(key)) {
+                    contents1[key]++;
+                }
+                else {
+                    contents1.Add(key, 1);
+                }
+            }
+
+            return contents1;
+        }
+
+
+        public Dictionary<string, int> contents(int compartmentId) {
+
+            Dictionary<string, int> thingsInCompartment = new Dictionary<string, int>();
+
+            string compartment;
+            if (compartmentId == 1) { compartment = this.compartment1; }
+            else { compartment = this.compartment2; }
+
+            for (int i = 0; i < compartment.Length; i++) {
+
+                string currentThing = compartment.Substring(i, 1);
+                if (thingsInCompartment.ContainsKey(currentThing)) {
+                    thingsInCompartment[currentThing]++;
+                }
+                else {
+                    thingsInCompartment.Add(currentThing, 1);
+                }
+            }
+
+            return thingsInCompartment;
+        }
+    }
+}
