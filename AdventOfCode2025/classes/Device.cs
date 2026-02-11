1b6d40a88f9300f66721964979ed251ec9e6acc7
mc <mc@madronahouse.com>
2026-02-10 20:36:20 -0800

    day 11

---
+++ AdventOfCode2025/classes/Device.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2025 {
+
+    internal class Device {
+
+        private DeviceManager deviceManager;
+        public List<string> outputs = new List<string>();
+        public List<Device> outputDevices = new List<Device>();
+        public string name;
+        public long value = -1;
+
+        public Device(string input, DeviceManager deviceManager) {
+
+            this.deviceManager = deviceManager; 
+
+            // sample format: "ccc: ddd eee fff"
+            string[] parts = input.Split(':');
+            this.name = parts[0];
+
+            string[] outputsAsString = parts[1].Substring(1).Split(' ');
+            foreach (string output in outputsAsString) {
+                if (output.Trim() != "") {
+                    outputs.Add(output);
+                }
+            }
+        }
+
+        public void Print() {
+
+            Console.Write(this.name + ": ");
+            foreach (string output in this.outputs) {
+                Console.Write(output + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        public long FindPaths2(List<Device> history, string outString) {
+
+            long successfulPaths = 0;
+
+            // do we have a cache for this?
+            if (this.value != -1) {
+                return this.value;
+            }
+
+            // Prepare a new history stack, if necessary.
+            List<Device> newHistory = new List<Device>();
+            foreach (Device historyDevice in history) {
+                newHistory.Add(historyDevice);
+            }
+            newHistory.Add(this);
+
+            // For each output, calculate the amount of successful paths out.
+            foreach (Device outputDevice in this.outputDevices) {
+
+                if (outputDevice.name == outString) {
+                    successfulPaths++;
+                }
+                else if (history.Contains(outputDevice)) {
+                    successfulPaths += 0;
+                }
+                else {
+                    successfulPaths += outputDevice.FindPaths2(newHistory, outString);
+                }
+            }
+
+            // cache this.
+            this.value = successfulPaths;
+
+            return successfulPaths;
+        }
+
+        //public int FindPaths(List<Device> history) {
+
+        //    int successfulPaths = 0;
+
+        //    // Prepare a new history stack, if necessary.
+        //    List<Device> newHistory = new List<Device>();
+        //    foreach (Device historyDevice in history) {
+        //        newHistory.Add(historyDevice);
+        //    }
+        //    newHistory.Add(this);
+
+        //    // For each output, calculate the amount of successful paths out.
+        //    foreach (Device outputDevice in this.outputDevices) {
+
+        //        if (outputDevice.name == "out") {
+        //            successfulPaths++;
+        //        }
+        //        else if (history.Contains(outputDevice)) {
+        //            successfulPaths += 0;
+        //        }
+        //        else {
+        //            successfulPaths += outputDevice.FindPaths(newHistory);
+        //        }
+        //    }
+
+        //    return successfulPaths;
+        //}
+    }
+}
---
+++ AdventOfCode2025/classes/DeviceManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AdventOfCode2025 {
+
+    internal class DeviceManager {
+
+        public List<Device> devices = new List<Device>();
+        public Device start = null;
+        public Device dac = null;
+        public Device fft = null;
+
+        public DeviceManager(string[] input, string firstNodeName) {
+
+            // initialize device list.
+            foreach (string inputItem in input) {
+
+                Device device = new Device(inputItem, this);
+                this.devices.Add(device);
+
+                // Find our special named ones.
+                if (device.name == firstNodeName) {
+                    this.start = device;
+                }
+                else if (device.name == "dac") {
+                    this.dac = device;
+                }
+                else if (device.name == "fft") {
+                    this.fft = device;
+                }
+            }
+
+            // Add the final one, 'out'.
+            this.devices.Add(new Device("out: ", this));
+
+            if (this.start == null) {
+                Debug.Assert(false, "failed to find start node.");
+            }
+
+            // Let's go in and fix up the device output lists.
+            foreach (Device device in this.devices) {
+                foreach (string output in device.outputs) {
+                    Device deviceOutput = this.devices.Find(x => x.name == output);
+                    if (deviceOutput == null) { int x = 5; }
+                    device.outputDevices.Add(deviceOutput);
+                }
+            }
+        }
+
+        public void Print() {
+
+            foreach (Device device in this.devices) {
+                device.Print(); 
+            }
+        }
+
+        public long FindPaths() {
+
+            //return this.start.FindPaths(new List<Device>());
+            return this.start.FindPaths2(new List<Device>(), "out");
+
+        }
+
+        private void ResetAllCachedValues() {
+
+            foreach (Device device in this.devices) {
+                device.value = -1;
+            }
+        }
+
+        public long FindPaths2() {
+
+            // If you don't have the important nodes, bail.
+            if (this.start == null || this.dac == null || this.fft == null) {
+                Console.WriteLine("Missing an important node for part two, bailing.");
+                return -1;
+            }
+
+            // start - dac - fft - end
+            long countStartToDac = this.start.FindPaths2(new List<Device>(), "dac");
+            this.ResetAllCachedValues();
+            long countDacToFft = this.dac.FindPaths2(new List<Device>(), "fft");
+            this.ResetAllCachedValues();
+            long countFftToEnd = this.fft.FindPaths2(new List<Device>(), "out");
+            this.ResetAllCachedValues();
+            long countStartToDacToFftToEnd = countStartToDac * countDacToFft * countFftToEnd;
+
+            long countStartToFft = this.start.FindPaths2(new List<Device>(), "fft");
+            this.ResetAllCachedValues();
+            long countFftToDac = this.fft.FindPaths2(new List<Device>(), "dac");
+            this.ResetAllCachedValues();
+            long countDacToEnd = this.dac.FindPaths2(new List<Device>(), "out");
+            this.ResetAllCachedValues();
+            long countStartToFftToDacToEnd = countStartToFft * countFftToDac * countDacToEnd;
+
+            return countStartToDacToFftToEnd + countStartToFftToDacToEnd;
+        }
+    }
+}
